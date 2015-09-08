aa261e5fcb38fe2c392ca1fdd8b39561826e2558
Tim Faulkes <tim.faulkes@gmail.com>
2015-09-08 17:24:38 -0600

    Updated the example to cater for sequences of lists. That is the list in the events bin is now bounded in size controlled by the MAX_RECORDS_IN_SEQUENCE value. Once more than this number of records is attempted to be written, a new key will be added with a sequence number dictated by the number of records.
    For example, assume MAX_RECORDS_IN_SEQUENCE is 100, and 100 records for User-125 have
    been written on 09-08-2015. There will be a key in the database with:
    User-125:09-08-2015:1
    
    However, when the next record is written an additional records will be in the database,
    so there will now be:
    User-125:09-08-2015:1
    User-125-09-08-2015:2
    
    Hence, no single entry in the database will store more than MAX_RECORDS_IN_SEQUENCE values.

--- AerospikeTraining/Program.cs
+++ AerospikeTraining/Program.cs
@@ -36,6 +36,10 @@ namespace AerospikeTraining
 		public const int MAX_DAYS = 60;
 		public const int BIG_EVENTS = 1000;
 		public const int SMALL_EVENTS = 10;
+		// We want to limit the number of records in one sequence so we don't overflow the size of a block (~128k by default)
+		// We have set this value artificially low to enable testing. Depending on blocksize and size of the entry in the list
+		// this number could be much higher in a real application.
+		public const int MAX_RECORDS_PER_SEQUENCE = 100;
 
 		public const string EVENT_STRING = "1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111" +
 		                                   "2222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222" +
@@ -74,37 +78,43 @@ namespace AerospikeTraining
 //				p = new Program(new AerospikeClient(policy, "C-9a8d04af83.aerospike.io", 3200));
 				p = new Program(new AerospikeClient(policy, "127.0.0.1", 3000));
 
+				int feature;
+				do {
+					
+	                // Present options
+	                Console.WriteLine("What would you like to do:");
+	                Console.WriteLine("1> Generate data");
+	                Console.WriteLine("2> Simulate daily events");
+	                Console.WriteLine("3> Get a day range of events for a user");
+	                Console.WriteLine("0> Exit");
+	                Console.Write("\nSelect 1-3 and hit enter:");
+					feature = int.Parse(Console.ReadLine());
 
-                // Present options
-                Console.WriteLine("What would you like to do:");
-                Console.WriteLine("1> Generate data");
-                Console.WriteLine("2> Simulate daily events");
-                Console.WriteLine("3> Get a day range of events for a user");
-                Console.WriteLine("0> Exit");
-                Console.Write("\nSelect 1-3 and hit enter:");
-				int feature = int.Parse(Console.ReadLine());
-
-                if (feature != 0)
-                {
-                    switch (feature)
-                    {
-                        case 1:
-                            Console.WriteLine("********** Generate Data");
-                            p.GenerateData();
-                            break;
-                        case 2:
-						Console.WriteLine("********** Simulate daily events");
-                            p.SimulateDailyEvents();
-                            break;
-                        case 3:
-						Console.WriteLine("********** Get a day range of events for a user");
-                            p.DayRangeForUser();
-                            break;
-                        default:
-                            Console.WriteLine("********** Invalid Selection ");
-                            break;
-                    }
-                }
+	                if (feature != 0)
+	                {
+	                    switch (feature)
+	                    {
+	                        case 1:
+	                            Console.WriteLine("********** Generate Data");
+	                            p.GenerateData();
+	                            break;
+	                        case 2:
+								Console.WriteLine("********** Simulate daily events");
+	                            p.SimulateDailyEvents();
+	                            break;
+	                        case 3:
+								Console.WriteLine("********** Get a day range of events for a user");
+	                            p.DayRangeForUser();
+	                            break;
+							case 0:
+								Console.WriteLine("Goodbye");
+								break;
+	                        default:
+	                            Console.WriteLine("********** Invalid Selection ");
+	                            break;
+	                    }
+	                }
+				} while (feature != 0);
             }
             catch (AerospikeException e)
             {
@@ -164,8 +174,9 @@ namespace AerospikeTraining
 					for (int z = 1; z <= NoOfEvents; z++) {
 						EventList.Add (z + ":" + EVENT_STRING);
 					}
+					Bin recordCount = new Bin ("total-records", NoOfEvents);
 					Bin Events = new Bin("events", EventList);
-					client.Put (null, EventKey, User, Day, Sequence, Events);
+					client.Put (null, EventKey, User, Day, Sequence, recordCount, Events);
 					DayRecords++;
 					EventRecords += NoOfEvents;
 
@@ -176,25 +187,92 @@ namespace AerospikeTraining
 				}
 			}
 		}
+		/*
+		 * Return the one-based sequence number for a record, assuming a maximum of MAX_RECORDS_PER_SEQUENCE
+		 * records will fit into one sequence record.
+		 */
+		private int GetSequenceNumberFromRecordNumber(int recordCount) {
+			return (recordCount / MAX_RECORDS_PER_SEQUENCE) + 1;
+		}
+
+		private String FormKeyString(String user, String date, int sequenceNumber) {
+			return user + ":" + date + ":" + sequenceNumber;
+		}
+
+		/*
+		 * Add an event to the specified user on the specified day. If there
+		 * are no events for this user on that day, a new user record will be created.
+		 */
 		public void AddEvent(string user, DateTime day, string EventString){
 			List<Object> EventList = null;
 			String DateString = day.ToString ("yyyy-MM-dd");
-			String EventKeyString = user + ":" + DateString + ":" + 1;
+			String EventKeyString = FormKeyString(user, DateString, 1);
 			Key EventKey = new Key ("test", "user-events", EventKeyString);
-			Record eventRecord = client.Get (null, EventKey, "events");
+			Record eventRecord = client.Get (null, EventKey, "events", "total-records");
 			if (eventRecord != null) {
+				// Add a new event with a generation count for optimistic concurrency
+				int currentCount = eventRecord.GetInt("total-records");
+				if (currentCount < 0) {
+					// Another thread has this record locked. Either retry or throw an exception
+					throw new AerospikeException ("Record locked");
+				}
 				updatePolicy.generation = eventRecord.generation;
-				EventList = (List<Object>)eventRecord.GetValue ("events");
-				EventList.Add (EventString);
-				client.Put (updatePolicy, EventKey, new Bin ("events", EventList));
+				// Determine the sequence number to write this record to
+				int sequenceNumber = GetSequenceNumberFromRecordNumber(currentCount);
+				if (sequenceNumber == 1) {
+					// No need to lock, write to the base record
+					EventList = (List<Object>)eventRecord.GetValue ("events");
+					EventList.Add (EventString);
+					Bin recordsCount = new Bin ("total-records", currentCount + 1);
+					client.Put (updatePolicy, EventKey, recordsCount, new Bin ("events", EventList));
+
+				} else {
+					// we need to write a lock to the "head" (sequence 1) record, then write
+					// the data, then finally unlock the head record and correct it's count
+					client.Put (updatePolicy, EventKey, new Bin ("total-records", -1));
+
+					try {
+						// Now write the data, just as a single bucket. Thus, we can always do a replace.
+						Key keyForSequence = new Key("test", "user-events", FormKeyString (user, DateString, sequenceNumber));
+						Record eventSequenceRecord = client.Get (null, keyForSequence, "events");
+						List<Object> eventSequenceList;
+						if (eventSequenceRecord == null || eventSequenceRecord.GetValue ("events") == null) {
+							// This is a new record
+							eventSequenceList = new List<Object> ();
+						} else {
+							eventSequenceList = (List<Object>)eventSequenceRecord.GetValue ("events");
+						}
+						eventSequenceList.Add (EventString);
+						Bin User = new Bin ("user-id", user);
+						Bin Day = new Bin ("day", DateString);
+						Bin Sequence = new Bin ("sequence", sequenceNumber);
+						Bin events = new Bin ("events", eventSequenceList);
+
+						WritePolicy sequencePolicy = new WritePolicy ();
+						sequencePolicy.recordExistsAction = RecordExistsAction.REPLACE;
+						client.Put (sequencePolicy, keyForSequence, User, Day, Sequence, events);
+
+						// Finally, unlock the record and update the count on it.
+						updatePolicy.generation = updatePolicy.generation+1;
+						client.Put (updatePolicy, EventKey, new Bin ("total-records", currentCount + 1));
+					}
+					catch (Exception) {
+						// An error occurred, release the lock
+						client.Put (updatePolicy, EventKey, new Bin ("total-records", currentCount));
+						throw;
+					}
+				}
 			} else {	
+				// Create a new event with CREATE_ONLY in case someone else has already created it.
 				Bin User = new Bin ("user-id", user);
 				Bin Day = new Bin ("day", DateString);
 				Bin Sequence = new Bin ("sequence", 1);
+				Bin TotalRecords = new Bin ("total-records", 1);
 				EventList = new List<Object> ();
 				EventList.Add (EventString);
-				client.Put (createPolicy, EventKey, User, Day, Sequence, new Bin ("events", EventList));
+				client.Put (createPolicy, EventKey, User, Day, Sequence, TotalRecords, new Bin ("events", EventList));
 			}
+			Console.WriteLine ("Wrote event: " + EventKeyString + ":" + EventString.Substring (0, 10) + "...");
 		}
 		public void SimulateDailyEvents(){
 
@@ -238,10 +316,15 @@ namespace AerospikeTraining
 			string user;
 			Console.WriteLine("Enter user (User-1):");
 			user = Console.ReadLine().Trim();
-			Console.WriteLine("\nEnter start day (1 is today, 365 is 1 year ago):");
+			Console.WriteLine("\nEnter start day (0 is today, 365 is 1 year ago):");
 			min = int.Parse(Console.ReadLine());
 			Console.WriteLine("Enter end day:");
 			max = int.Parse(Console.ReadLine());
+			if (min > max) {
+				int temp = max;
+				max = min;
+				min = temp;
+			}
 			List<Key> KeyList = new List<Key> ();
 			for (int y = min; y <= max; y ++) {
 				DateTime EventDay = epoch.Subtract (TimeSpan.FromDays (y));
@@ -263,7 +346,30 @@ namespace AerospikeTraining
 		}
 
 		private string EventToString(Record eventRecord){
+			// Note: If we want to get a list of all the records to iterate through rather than just the count,
+			// we get the maximum sequence from the count, iterate through the keys from 2->Max Sequence (as we
+			// already have record 1), form a list of keys then do a batch read. This code is provided for clarity
+			//
+			// We probably should be using locking to make sure we have no concurrency issues: see AddEvent method
+			// for example locking code.
+			String user = eventRecord.GetString("user-id");
+			String dateStr = eventRecord.GetString ("day");
 			List<Object> events = (List<Object>) eventRecord.GetValue ("events");
+			int count = eventRecord.GetInt ("total-records");
+			List<Key> keyList = new List<Key> ();
+			for (int i = 2; i <= GetSequenceNumberFromRecordNumber (count); i++) {
+				keyList.Add(new Key ("test", "user-events", FormKeyString(user, dateStr, i)));	
+				Key[] keys = keyList.ToArray ();
+				Record[] records = client.Get (null, keys);
+				foreach (Record record in records) {
+					if (record != null) {
+						List<Object> theseEvents = (List<Object>)record.GetValue ("events");
+						events.AddRange(theseEvents);
+					}
+				}
+
+			}
+				
 			String result = eventRecord.GetString ("user-id") + ":" + eventRecord.GetString ("day") + ":" + eventRecord.GetInt ("sequence") + ":" + events.Count;
 			return result;
 		}
